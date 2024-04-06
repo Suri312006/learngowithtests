839d7e918cbb743ce69b786c2542cc2152b681e0
suri312006 <suri312006@gmail.com>
2024-04-06 10:43:47 -0700

    mane i m just a follower

--- http_server/main.go
+++ http_server/main.go
@@ -1,19 +1,12 @@
+// main.go
 package main
 
 import (
-    "log"
-    "net/http"
- "github.com/suri312006/learnGoWithTests/http_server/server"   
+	"log"
+	"net/http"
 )
 
-type InMemoryPlayerStore struct{}
-
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-    return 123
-}
-
-
-func main(){
-    server := &httpserver.PlayerServer{&InMemoryPlayerStore{}}
-    log.Fatal(http.ListenAndServe(":5000", server))
+func main() {
+	server := NewPlayerServer(NewInMemoryPlayerStore())
+	log.Fatal(http.ListenAndServe(":5000", server))
 }
